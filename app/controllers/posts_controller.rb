677a926c2024eec4c8900929200ad72599ebc262
MuhammadBilalRiaz <bilalmirzase@gmail.com>
2018-05-11 16:10:04 +0500

    comment work done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,7 +24,6 @@ class PostsController < ApplicationController
   # POST /posts
   # POST /posts.json
   def create
-    debugger
     @post = current_user.posts.build(post_params)
 
     respond_to do |format|
